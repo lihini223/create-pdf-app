f13401a9f4fc02803567e156e8a02dd198f33d5e
lihini223 <lihininisansala95@gmail.com>
2021-04-10 21:48:57 +0530

    add create function

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from pywebio.input import textarea, input
 from pywebio import start_server
 from pywebio.output import output
+from fpdf import FPDF
 
 
 def app_main():
@@ -10,6 +11,18 @@ def app_main():
     save_location = input("Enter a name for your input file",
                           required=True)
     put_text("Tadaaa.." + save_location + "pdf is generated.")
+
+
+def create_pdf():
+    pdf = FPDF()
+    pdf.add_page()
+    pdf.set_font('arial', '', 16)
+
+    lines = text.split('\n')
+    for i, sent in enumerate(lines):
+        pdf.cell(40, 10, sent, 0, i + 1)
+
+    pdf.output(save_location, 'F')
 
 
 if __name__ == '__main__':
